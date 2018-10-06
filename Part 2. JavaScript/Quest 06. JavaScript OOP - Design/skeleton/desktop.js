f23169d2113b60fb36637ebb2c4b0f6bdec09242
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-06 21:54:07 +0900

    Complete Quest 06

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,15 +1,42 @@
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(){
+        // 이름, 위치
+    }
+
+    makeNewIcon(){} // 아이콘 생성
+    sort(){} // 정렬
 };
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(){
+        // 이름, 위치, 종류, 아이콘 모양 크기, 용량, 생성일시, 변경일시, 생성자명
+    }
+
+    open(){} // 더블클릭하면 창 오픈
+    drag(){} // 드래그하여 움직이기
+    delete(){} // 아이콘 삭제
+    changeName(){} // 이름 변경
+    copy(){} // 복사
 };
 
 class Folder {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(){
+        // 이름
+    }
+
+    showContents(){} // 폴더 내 컨텐츠 띄우기
+    changeDisplay(){} // 폴더 내 컨텐츠 표현 방식 변경
 };
 
 class Window {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+    constructor(){
+        // 창(화면)의 크기, 높이
+    }
+
+    close(){} // 창 닫기
+    resize(){} // 창 사이즈 조절
 };
