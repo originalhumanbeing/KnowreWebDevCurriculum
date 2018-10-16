d268fc83ad6df0b1d2adeb7f3f92fc8079a5e1c4
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-16 23:19:37 +0900

    Complete Quest 10.

--- Part 3. node.js/Quest 11. My little web server/README.md
+++ Part 3. node.js/Quest 11. My little web server/README.md
@@ -15,9 +15,11 @@
 
 ## Checklist
 * HTTP의 GET과 POST 메소드는 어떻게 다른가요?
+  * `GET`:
+  * `POST`:
   * 다른 HTTP 메소드에는 무엇이 있나요?
-    * `PUT`
-    * `DELETE`
+    * `PUT`: 
+    * `DELETE`: 
 * HTTP 서버에 GET과 POST를 통해 데이터를 보내려면 어떻게 해야 하나요?
   * HTTP 요청의 `Content-Type` 헤더는 무엇인가요?
   * Postman에서 POST 요청을 보내는 여러 가지 방법(`form-data`, `x-www-form-urlencoded`, `raw`, `binary`) 각각은 어떤 용도를 가지고 있나요?
@@ -26,5 +28,12 @@
 * 다음의 동작을 하는 서버를 만들어 보세요.
   * 브라우저의 주소창에 `http://localhost:8080`을 치면 `Hello World!`를 응답하여 브라우저에 출력하는 서버를 만들어 보세요.
   * 서버의 `/foo` URL에 `bar` 변수로 임의의 문자열을 GET 메소드로 보내면, `Hello, [문자열]`을 출력하는 서버를 만들어 보세요.
+  ![get](./result/q10_get.png)
+  
   * 서버의 `/foo` URL에 `bar` 변수로 임의의 문자열을 POST 메소드로 보내면, `Hello, [문자열]`을 출력하는 서버를 만들어 보세요.
+  ![post](./result/q10_post.png)
+  
+  * `/`인 경우
+  ![main](./result/q10_main.png)
+
 * expressJS와 같은 외부 프레임워크를 사용하지 않고 만들어 보세요.
--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,7 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 http.createServer((req, res) => {
-	// TODO: 이 곳을 채워넣으세요..!
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end('Hello World!');
+    let query = url.parse(req.url, true).query;
+    let bar = JSON.stringify(query['bar']);
+    let pathname = url.parse(req.url, true).pathname;
+
+    if(req.method == 'GET' && pathname == '/foo') {
+       res.writeHead(200, {
+            'Content-Type' : 'text/html'});
+       res.end('<h1>Hello '+bar+' (GET Method)</h1>');
+    } else if(req.method == 'POST' && pathname == '/foo') {
+        req.on('data', (data)=> {
+            res.writeHead(200, {
+                'Content-Type' : 'text/html'});
+            res.end('<h1>Hello, '+ data + ' ' + bar+' (POST Method)</h1>');
+        });
+    } else {
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'});
+        res.end('<h1>Hello, this is main page</h1>');
+    }
 }).listen(8080, 'localhost');
