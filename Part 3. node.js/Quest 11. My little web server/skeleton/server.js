d2a7b9edba095ecbf04ef5ec12faf2e3c45173bf
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-18 23:41:21 +0900

    Succeed making client and server communicating GET/POST method

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -4,28 +4,27 @@ const fs = require('fs');
 
 http.createServer((req, res) => {
     let query = url.parse(req.url, true).query;
-    let bar = JSON.stringify(query['bar']);
     let pathname = url.parse(req.url, true).pathname;
+    let body = '';
 
-    if (req.method == 'GET' && pathname == '/foo') {
-        console.log(url.parse(req.url));
-        console.log('query', query);
-        console.log('bar', bar);
-        console.log('pathname', pathname);
+    if (req.method === 'GET' && pathname === '/foo') {
+        let bar = JSON.stringify(query['bar']);
+        bar = bar.substr(1,3);
+        body = 'Hello ' + bar;
         res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        res.end('<h1>Hello ' + bar + ' (GET Method)</h1>');
-    } else if (req.method == 'POST' && pathname == '/foo') {
-        console.log('POST 받았다');
-        let body = 'POST 통신 성공';
+        res.end(body);
+    } else if (req.method === 'POST' && pathname === '/foo') {
         req.on('data', (data) => {
-            body = data.toString()
+            body += data.toString();
         }).on('end', () => {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
             res.end(body);
         });
-    }
-    else {
+    } else {
         fs.readFile('index.html', function (error, data) {
             if (data) {
                 res.writeHead(200, {
@@ -33,7 +32,7 @@ http.createServer((req, res) => {
                 });
                 res.end(data);
             } else {
-                console.log('에러다');
+                console.log(error);
             }
         })
     }
