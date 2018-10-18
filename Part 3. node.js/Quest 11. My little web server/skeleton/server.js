c70c100a8d08c24f5360c7d7742e7bafe92d45ab
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-18 19:14:52 +0900

    Create server, implement method related with HTTP request (POST), response

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,24 +1,40 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 http.createServer((req, res) => {
     let query = url.parse(req.url, true).query;
     let bar = JSON.stringify(query['bar']);
     let pathname = url.parse(req.url, true).pathname;
 
-    if(req.method == 'GET' && pathname == '/foo') {
-       res.writeHead(200, {
-            'Content-Type' : 'text/html'});
-       res.end('<h1>Hello '+bar+' (GET Method)</h1>');
-    } else if(req.method == 'POST' && pathname == '/foo') {
-        req.on('data', (data)=> {
-            res.writeHead(200, {
-                'Content-Type' : 'text/html'});
-            res.end('<h1>Hello, '+ data + ' ' + bar+' (POST Method)</h1>');
-        });
-    } else {
+    if (req.method == 'GET' && pathname == '/foo') {
+        console.log(url.parse(req.url));
+        console.log('query', query);
+        console.log('bar', bar);
+        console.log('pathname', pathname);
         res.writeHead(200, {
-            'Content-Type' : 'text/html'});
-        res.end('<h1>Hello, this is main page</h1>');
+            'Content-Type': 'text/html'
+        });
+        res.end('<h1>Hello ' + bar + ' (GET Method)</h1>');
+    } else if (req.method == 'POST' && pathname == '/foo') {
+        console.log('POST 받았다');
+        let body = 'POST 통신 성공';
+        req.on('data', (data) => {
+            body = data.toString()
+        }).on('end', () => {
+            res.end(body);
+        });
+    }
+    else {
+        fs.readFile('index.html', function (error, data) {
+            if (data) {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.end(data);
+            } else {
+                console.log('에러다');
+            }
+        })
     }
 }).listen(8080, 'localhost');
