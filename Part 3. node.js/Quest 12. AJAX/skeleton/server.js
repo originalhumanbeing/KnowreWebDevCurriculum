e1df7c222e0bbbf8f27241505742f41684e17ee9
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-19 16:29:21 +0900

    Attach Notepad/Tab nodes on body tag

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
 });
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+// path가 파일명이 되서 그거대로 라우팅하나...?
 
 const server = app.listen(8080, () => {
 	console.log('Server started!');
