e961c50c58ac059650ec11b5531cf5be50e32f14
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-21 18:37:32 +0900

    Study MySQL, MySQL module

---
+++ Part 3. node.js/node_prac/db_module/mysql_example.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+    user: 'root',
+    password: 'rhforhfo89!'
+})
+
+client.query('USE Company');
+client.query('SELECT * FROM products', function(error, result, fields) {
+    if(error) {
+        console.log(error);
+        console.log('쿼리 문장에 오류가 있습니다');
+    } else {
+        console.log(result);
+    }
+});
