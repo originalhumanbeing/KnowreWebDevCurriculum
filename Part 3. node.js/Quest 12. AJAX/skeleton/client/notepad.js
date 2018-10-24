7315f91400dabd3047c98c6171b3d7138dc76cc4
originalhumanbeing <han.hyunjung17@gmail.com>
2018-10-24 19:50:21 +0900

    Refactor Read all memos, Create memo function from express, Add related codes on client side (showing list/ saving current memo)

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,3 +1,52 @@
-class Notepad1 {
-	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-};
+class Notepad {
+    constructor() {
+        this.writeBtn = document.querySelector('.writeBtn');
+        this.saveBtn = document.querySelector('.saveBtn');
+        this.updateBtn = document.querySelector('.updateBtn');
+        this.deleteBtn = document.querySelector('.deleteBtn');
+        this.textarea = document.querySelector('.memo');
+        this.list = document.querySelector('.list');
+        this.currentFile = '';
+
+        this.showList();
+        this.addClickEvents();
+    }
+
+    showList() {
+        // 변경 사항이 있을 때마다 새로 뿌리는 것은...?
+        fetch(`http://localhost:8080/memos`, {
+            method: 'get'
+        }).then((res) => res.json()).then((data) => {
+            for(let memo of data) {
+                let li = document.createElement('li');
+                li.innerText = memo;
+                this.list.appendChild(li);
+            }
+        })
+    }
+
+    addClickEvents() {
+        this.saveBtn.addEventListener('click', () => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "text/plain");
+
+            fetch(`http://localhost:8080/memo`, {
+                method: 'post',
+                headers: myHeaders,
+                body: this.textarea.value
+            //  문자열을 JSON.stringify로 감싸면 "" 가 생김
+            //  req 보낼 때, 서버에서 res 보낼 때 항상 문자열로 보내고 받을 때는 버퍼 영역에 있던 바이너리 형태의 데이터를 객체화한다
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    this.textarea.value = data.body;
+                    this.currentFile = data.title;
+                })
+        });
+
+        this.updateBtn.addEventListener('click', () => {
+            console.log(this.currentFile);
+        })
+    }
+}
+
--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -4,10 +4,9 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 app.use(express.static('client'));
 
 app.get('/', (req, res) => {
@@ -19,14 +18,10 @@ app.get('/', (req, res) => {
 // 전체 메모 리스트 가져오기
 app.get('/memos', function (req, res) {
     fs.readdir('./memos', function (err, files) {
-        console.log(typeof files);
-        console.log(files);
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end(files.toString());
-        })
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(JSON.stringify(files));
     });
+});
 
 // 메모 읽기
 app.get('/memo/:title', function (req, res) {
@@ -45,25 +40,25 @@ app.get('/memo/:title', function (req, res) {
 });
 
 // 새 메모 저장
-let count = 0;
 app.post('/memo', function (req, res) {
-    let data = req.body.body;
-    count++;
-    if(data) {
-        let title = `./memos/${count}.txt`;
-        fs.writeFile(title, data, function (error) {
-            if(error) {
-                console.log(error);
-            } else {
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                fs.readFile(title, 'utf8', function (error, data) {
-                   res.end(data);
-                });
-            }
-        })
-    }
+    let data = req.body;
+    if (!data) return res.sendStatus(400);
+
+    fs.readdir('./memos', function (err, files) {
+        let totalFiles = files.length;
+        let title = totalFiles + 1;
+        let fileLocation = `./memos/${title}.txt`;
+
+        fs.writeFile(fileLocation, data, function(error) {
+            if(error) throw error;
+        });
+
+        fs.readFile(fileLocation, 'utf8', function(error, data) {
+            if(error) throw error;
+            res.writeHead(200, { 'Content-Type' : 'text/html' });
+            res.end(JSON.stringify( { body: data, title: fileLocation }));
+        });
+    });
 });
 
 // 메모 수정
