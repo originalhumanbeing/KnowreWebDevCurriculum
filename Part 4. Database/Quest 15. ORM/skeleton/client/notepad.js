a7d1b6bb957b1371fde18c924d817023fd1207f9
originalhumanbeing <han.hyunjung17@gmail.com>
2018-11-06 23:50:51 +0900

    Revert "Refactor to load memo that user worked on last time with cursor!"
    This reverts commit 5797a2e

--- Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
+++ Part 4. Database/Quest 15. ORM/skeleton/client/notepad.js
@@ -36,22 +36,8 @@ class Notepad {
             headers: myHeaders,
             body: JSON.stringify({id: id, pwd: pwd})
         }).then((res) => res.json()).then((data) => {
-                console.log('session 객체', data);
-            if (data['body'] && data['body'].isLogin && data['body'].nickname) {
-                this.textarea.value = '';
-                this.currentFile = '';
+            if (data['body'].isLogin && data['body'].nickname) {
                 this.currentUser = data['body'].nickname;
-                this.showList();
-                this.authForm.hidden = true;
-                this.userNickname.innerText = this.currentUser + '님';
-                this.userNav.hidden = false;
-                this.authFailMsg.hidden = true;
-            } else if (data['session'] && data['session'].isLogin && data['session'].nickname && data['lastMemoContent']) {
-                this.textarea.value = data['lastMemoContent'].content || '';
-                this.currentFile = data['lastMemoContent'];
-                this.textarea.setSelectionRange(data['lastMemoContent'].cursorStart, data['lastMemoContent'].cursorEnd);
-                this.textarea.focus();
-                this.currentUser = data['session'].nickname;
                 this.showList();
                 this.authForm.hidden = true;
                 this.userNickname.innerText = this.currentUser + '님';
